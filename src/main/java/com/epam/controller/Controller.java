c77656e90b0f113fe25cc87b99d34ed84f65d226
Asset Kenezhanov <bysskc@mail.ru>
2019-04-10 10:46:14 +0600

    3-commit(AS config files and getting DataSource by JNDI context)

--- src/main/java/com/epam/controller/Controller.java
+++ src/main/java/com/epam/controller/Controller.java
@@ -1,17 +1,74 @@
 package com.epam.controller;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Controller extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Context context = null;
+        Long c = 0L;
+
+        try {
+            context = new InitialContext();
+
+            //adding and getting objects from JNDI context.
+            context.bind("counter", c);
+            NamingEnumeration namesList = context.list("");
+            while(namesList.hasMoreElements())
+            {
+                Object object = namesList.nextElement();
+                System.out.println(object);
+            }
+            c = (Long) context.lookup("counter");
+            System.out.println(c);
+            c++;
+            context.rebind("counter", c);
+
+
+            //Getting DataSource from JNDI context and using it.
+            DataSource dataSource = (DataSource) context.lookup("java:jboss/datasources/OracleDS");
+            System.out.println("datasource: " + dataSource);
+            try {
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM NEWS");
+                ResultSet reesult =  preparedStatement.executeQuery();
+                while (reesult.next())
+                {
+                    Long id = reesult.getLong(1);
+                    System.out.println(id);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                context.close();
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/page1.jsp");
+        requestDispatcher.forward(request, response);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
---
+++ src/main/webapp/WEB-INF/page1.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Asset_Kenezhanov
+  Date: 09-Apr-19
+  Time: 9:50 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h2>Page 1</h2>
+</body>
+</html>
